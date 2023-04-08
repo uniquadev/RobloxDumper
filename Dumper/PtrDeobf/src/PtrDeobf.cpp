9114527536789529fa6908f58a6530e7607ec2a0
uniquadev <uniquadev@gmail.com>
2023-04-08 21:17:33 +0100

    improved ptr deobfuscation

--- Dumper/PtrDeobf/src/PtrDeobf.cpp
+++ Dumper/PtrDeobf/src/PtrDeobf.cpp
@@ -1,6 +1,8 @@
 #include "PtrDeobf.h"
-
 #include "Zydis.h"
+
+#include <vector>
+#include <algorithm>
 
 std::string Dumper::PtrDeobf::get_ptrobf_type_str(uintptr_t text_start, size_t length)
 {
@@ -8,14 +10,14 @@ std::string Dumper::PtrDeobf::get_ptrobf_type_str(uintptr_t text_start, size_t l
 }
 
 
-PtrObfPosition get_ptrpos_fromlea(ZydisRegister left, ZydisRegister right, uintptr_t text_start, size_t lenght)
+PtrObfPosition get_ptrpos(ZydisRegister left, ZydisRegister right, uintptr_t text_start, size_t lenght)
 {
     ZyanU8* data = (ZyanU8*)text_start;
 
     ZyanUSize offset = 0;
     ZydisDisassembledInstruction instruction;
     while (ZYAN_SUCCESS(ZydisDisassembleIntel(
-        ZYDIS_MACHINE_MODE_LONG_64,
+        ZYDIS_MACHINE_MODE_LONG_COMPAT_32,
         text_start,
         data + offset,
         lenght - offset,
@@ -23,15 +25,27 @@ PtrObfPosition get_ptrpos_fromlea(ZydisRegister left, ZydisRegister right, uintp
     )))
     {
         offset += instruction.info.length;
-        if (instruction.info.opcode != 0x8D) // we look for lea operations on left or right reigsters
-            continue;
-        auto operand = instruction.operands[0];
-        if (operand.type != ZYDIS_OPERAND_TYPE_REGISTER)
-            continue;
-        if (operand.reg.value == left)
-            return PtrObfPosition::LEFT;
-        else if (operand.reg.value == right)
-            return PtrObfPosition::RIGHT;
+        if (instruction.info.opcode == 0x8D) // we look for lea operations on left or right registers
+        {
+            auto operand = instruction.operands[0];
+            if (operand.type != ZYDIS_OPERAND_TYPE_REGISTER)
+                continue;
+            if (operand.reg.value == left)
+                return PtrObfPosition::LEFT;
+            else if (operand.reg.value == right)
+                return PtrObfPosition::RIGHT;
+        }
+        else if (instruction.info.opcode == 0x8B)
+        {
+            // check if mov a, [b] case
+            if (instruction.operands[0].type != ZYDIS_OPERAND_TYPE_REGISTER || instruction.operands[1].type != ZYDIS_OPERAND_TYPE_MEMORY)
+                continue;
+            auto reg = instruction.operands[0].reg.value;
+            if (reg == left)
+                return PtrObfPosition::LEFT;
+            else if (reg == right)
+                return PtrObfPosition::RIGHT;
+        }
     }
     return PtrObfPosition::NONE_POSITION;
 }
@@ -40,11 +54,13 @@ PtrObfuscationType Dumper::PtrDeobf::get_ptrobf_type(uintptr_t text_start, size_
 {
     ZyanU8* data = (ZyanU8*)text_start;
 
+    std::vector<ZydisRegister> holding_imm = {};
+
     // Loop over the instructions in our buffer.
     ZyanUSize offset = 0;
     ZydisDisassembledInstruction instruction;
     while (ZYAN_SUCCESS(ZydisDisassembleIntel(
-        /* machine_mode:    */ ZYDIS_MACHINE_MODE_LONG_64,
+        /* machine_mode:    */ ZYDIS_MACHINE_MODE_LONG_COMPAT_32,
         /* runtime_address: */ text_start,
         /* buffer:          */ data + offset,
         /* length:          */ length - offset,
@@ -53,20 +69,26 @@ PtrObfuscationType Dumper::PtrDeobf::get_ptrobf_type(uintptr_t text_start, size_
     {
         switch (instruction.info.opcode)
         {
+        case 0xB8: // MOV, r32, imm32
+        {
+            holding_imm.push_back(instruction.operands[0].reg.value);
+            break;
+        }
         case 0x2B: //SUB r32, r/m32
-            return PtrObfuscationType::SUB_X_P; // b - *b
         case 0x29:  //SUB r/m32, r32
         {
             // this opcode accept:
             // #1 sub     edx, esi
-            // #2 sub     [edx], esi
+            // #2 sub     [edx], esi / edx, [esi]
             // #2 case is simple
             // #1 case require us to figure out which of the reg got deref
             // #2 case
             if (instruction.operands[0].type == ZYDIS_OPERAND_TYPE_MEMORY)
                 return PtrObfuscationType::SUB_P_X; // *b - b
+            else if (instruction.operands[1].type == ZYDIS_OPERAND_TYPE_MEMORY)
+                return PtrObfuscationType::SUB_X_P; // b - *b
             // #1 case
-            auto pos = get_ptrpos_fromlea(
+            auto pos = get_ptrpos(
                 instruction.operands[0].reg.value,
                 instruction.operands[1].reg.value,
                 text_start,
@@ -80,7 +102,16 @@ PtrObfuscationType Dumper::PtrDeobf::get_ptrobf_type(uintptr_t text_start, size_
             return PtrObfuscationType::NONE_TYPE;
         }
         case 0x03: //ADD r32, r/m32
+        {
+            // check if add is using a 2nd operand with immediate value
+            if (instruction.operands[1].type == ZYDIS_OPERAND_TYPE_REGISTER)
+            {
+                auto it = std::find(holding_imm.begin(), holding_imm.end(), instruction.operands[1].reg.value);
+                if (it != holding_imm.end())
+                    break;
+            }
             return PtrObfuscationType::ADD;
+        }
         case 0x33: //XOR r32, r/m32
             return PtrObfuscationType::XOR;
         default:
