de2cde1f1cc39917933b61817c9ad5769d4d2384
uniquadev <uniquadev@gmail.com>
2023-04-13 20:32:37 +0100

    better jmpesi dumper

--- Dumper/Jobs/src/Misc.cpp
+++ Dumper/Jobs/src/Misc.cpp
@@ -8,15 +8,15 @@ bool dump_misc(JobsHandler* h)
 {
 	h->push_addy("RobloxModule", r_module);
 
-	// retn
 	// jmp     esi
+	// any FF E6 in the text memory will be fine.
 	auto jmp_sign = scan(
-		"C3 FF E6",
+		"FF E6",
 		SearchSettings(r_text_start, r_text_end, PAGE_EXECUTE_READ, true, false)
 	);
 	if (jmp_sign.size() < 1)
 		JOBERROR(h, "Unable to find JmpEsi signature");
 
-	h->push_addy("JmpEsi", get_offset(jmp_sign[0] + 1));
+	h->push_addy("JmpEsi", get_offset(jmp_sign[0]));
 	return true;
 }
