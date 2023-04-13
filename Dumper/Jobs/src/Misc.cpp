a43746b4f2fc112fdc3c0c8db4fa55fbb253bce1
uniquadev <uniquadev@gmail.com>
2023-04-13 18:37:09 +0100

    fixed jmp esi dumper

--- Dumper/Jobs/src/Misc.cpp
+++ Dumper/Jobs/src/Misc.cpp
@@ -8,14 +8,15 @@ bool dump_misc(JobsHandler* h)
 {
 	h->push_addy("RobloxModule", r_module);
 
-	// jmp esi
+	// retn
+	// jmp     esi
 	auto jmp_sign = scan(
-		"FF E6 C3",
-		SearchSettings(r_text_start, r_text_end, PAGE_EXECUTE_READ, true, true)
+		"C3 FF E6",
+		SearchSettings(r_text_start, r_text_end, PAGE_EXECUTE_READ, true, false)
 	);
 	if (jmp_sign.size() < 1)
 		JOBERROR(h, "Unable to find JmpEsi signature");
 
-	h->push_addy("JmpEsi", get_offset(jmp_sign[0]));
+	h->push_addy("JmpEsi", get_offset(jmp_sign[0] + 1));
 	return true;
 }
